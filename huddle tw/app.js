ae4822780589e7f24614d8e48f726e5df362c832
Favour Peters <98953409+FavourPeters@users.noreply.github.com>
2022-09-20 23:02:13 +0100

    Add files via upload

---
+++ huddle tw/app.js
@@ -0,0 +1,9 @@
+// let vibes = document.body;
+// let wow = document.querySelector('.navbar')
+
+
+// vibes.addEventListener('mouseover', Click);
+
+// function Click(e){
+//     vibes.style.background = `rgb(${e.clientY}, ${e.clientX}, 80)`
+// }
